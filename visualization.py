81e1a6088c042a29f63d2902f524c08ca9811d55
91htjung <82224430+91htjung@users.noreply.github.com>
2025-05-01 21:43:01 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -12,6 +12,10 @@ def plot_documents_clusters(topic_model, docs, embeddings):
     fig = topic_model.visualize_document_datamap(docs, embeddings=embeddings, width=1800, height=900)
     return fig
 
+def plot_topic_bar(topic_model):
+     fig = topic_model.visualize_barchart()
+     return fig
+ 
 def plot_topics_over_time(topics_over_time_df, topic_model, top_n_topics=10):
     fig = topic_model.visualize_topics_over_time(topics_over_time_df, top_n_topics=top_n_topics)
     return fig
